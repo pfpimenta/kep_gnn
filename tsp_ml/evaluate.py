b2b7f3e748b7e210ce33003a54039fd18b37b722
pfpimenta <pedrofolettopimenta@gmail.com>
2022-09-02 16:28:09 -0300

    evaluate.py WIP

--- tsp_ml/evaluate.py
+++ tsp_ml/evaluate.py
@@ -3,10 +3,10 @@ import sys
 from typing import Optional
 
 import torch
-from dataset_utils import get_dataset, print_dataset_information
+from dataset_utils import get_dataset, get_instance_ids, print_dataset_information
 from model_utils import load_model, set_torch_seed
 from paths import get_predictions_folder_path
-from predict import create_predictions_CSV, save_predictions_to_csv
+from predict import initialize_predictions_CSV, save_predictions_to_csv
 from torch_geometric.data import Dataset
 from torch_geometric.loader import DataLoader
 from tqdm import tqdm
@@ -16,6 +16,50 @@ from model_performance import ModelPerformance
 DATASET_NAME = "KEP"
 TRAINED_MODEL_NAME = "2022_08_08_17h35_KEP_GCN"
 BATCH_SIZE = 10
+
+
+def evaluate_tsp_model(
+    model: torch.nn.Module,
+    dataset: Dataset,
+    batch_size: int,
+    output_filepath: Optional[str] = None,
+):
+    model_performance = ModelPerformance()
+    dataloader = DataLoader(
+        dataset, shuffle=False, batch_size=batch_size, pin_memory=True, num_workers=4
+    )
+    model.eval()  # set the model to evaluation mode
+
+    for i, batch in enumerate(tqdm(dataloader, desc="Evaluation", file=sys.stdout)):
+        batch = batch.to(device)
+
+        label = batch.y
+        label = label.to(torch.float32)
+
+        # get predictions
+        scores = model(data=batch)
+        # edge classification
+        pred = torch.argmax(scores, 1).to(int)
+
+        # update model_performance object with predictions
+        model_performance.update(pred=pred, truth=batch.y)
+
+        # TODO is solution valid?
+
+        # save predictions
+        if output_filepath:
+            # get instance IDs
+            instance_ids = get_instance_ids(batch=batch)
+            # save ids, predictions and y_truth (if not None) in a CSV
+            save_predictions_to_csv(
+                filepath=output_filepath,
+                instance_ids=instance_ids,
+                pred=pred,
+                truth=batch.y,
+            )
+
+    model_performance.print()
+    return model_performance
 
 
 def evaluate(
@@ -29,8 +73,10 @@ def evaluate(
 
     set_torch_seed()
 
+    print_dataset_information(dataset=dataset)
+
     # check if predictions file already exist
-    save_predictions, filepath = create_predictions_CSV(
+    save_predictions, output_filepath = initialize_predictions_CSV(
         output_dir=output_dir, dataset_name=dataset.name
     )
 
@@ -52,22 +98,30 @@ def evaluate(
         if dataset.dataset_name == "DTSP":
             # graph+cost classification
             pred = scores.to(int)  # DTSP
-        elif dataset.dataset_name == "TSP" or "KEP":
+        elif dataset.dataset_name == "TSP" or dataset.dataset_name == "KEP":
             # edge classification
             pred = torch.argmax(scores, 1).to(int)
-        model_performance.update(pred=pred, truth=batch.y)
 
+        if dataset.dataset_name == "TSP" or dataset.dataset_name == "DTSP":
+            # update model_performance object with predictions
+            model_performance.update(pred=pred, truth=batch.y)
+        elif dataset.dataset_name == "KEP":
+            pass
+
+        # TODO is solution valid?
+
+        # save predictions
         if save_predictions:
             # get instance IDs
-            row, _ = batch.edge_index
-            edge_batch = batch.batch[row]
-            instance_ids = [batch.id[idx] for idx in edge_batch.tolist()]
-            # save predictions in a CSV
+            instance_ids = get_instance_ids(batch=batch)
+            # save ids, predictions and y_truth (if not None) in a CSV
             save_predictions_to_csv(
-                filepath=filepath, instance_ids=instance_ids, pred=pred, truth=batch.y
+                filepath=output_filepath,
+                instance_ids=instance_ids,
+                pred=pred,
+                truth=batch.y,
             )
 
-    print_dataset_information(dataset=dataset)
     model_performance.print()
     return model_performance
 
@@ -86,7 +140,9 @@ if __name__ == "__main__":
 
     print("\n\nEvaluating the model on the train dataset")
     predictions_dir = get_predictions_folder_path(
-        dataset_name=DATASET_NAME, step="train"
+        dataset_name=DATASET_NAME,
+        step="train",
+        trained_model_name=TRAINED_MODEL_NAME,
     )
     train_model_performance = evaluate(
         model=model,
@@ -98,7 +154,9 @@ if __name__ == "__main__":
 
     print("\n\nEvaluating the model on the test dataset")
     predictions_dir = get_predictions_folder_path(
-        dataset_name=DATASET_NAME, step="train"
+        dataset_name=DATASET_NAME,
+        step="train",
+        trained_model_name=TRAINED_MODEL_NAME,
     )
     test_model_performance = evaluate(
         model=model,
