af13def530699e02fee711d79b8d635fbbcaf3c5
pfpimenta <pedrofolettopimenta@gmail.com>
2023-02-16 13:16:30 -0300

    updated requirements.txt

--- tsp_ml/import_test.py
+++ tsp_ml/import_test.py
@@ -3,9 +3,10 @@ import networkx as nx
 import numpy as np
 import torch
 import torch_geometric
-from python_tsp.exact import solve_tsp_dynamic_programming
 from torch_geometric.data import DataLoader
 from torch_geometric.utils.convert import from_networkx
 from tqdm import tqdm
+
+# from python_tsp.exact import solve_tsp_dynamic_programming # only needed for TSP
 
 print("...everything is fine! All imports worked!")
