62addcb14282ca66c15f31ce707ba72c518f223e
pfpimenta <pedrofolettopimenta@gmail.com>
2023-02-21 13:57:37 -0300

    added pycsp3 dependency + pre-commit update: black and isort compatibility

--- tsp_ml/import_test.py
+++ tsp_ml/import_test.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 import torch
 import torch_geometric
+from pycsp3 import *
 from torch_geometric.data import DataLoader
 from torch_geometric.utils.convert import from_networkx
 from tqdm import tqdm
