af89bd08a58af04c7e3f813bf8b0a0bebdd4435d
pfpimenta <pedrofolettopimenta@gmail.com>
2023-03-01 11:39:23 -0300

    eval_solver_time.py script update

--- tsp_ml/scripts/eval_solver_time.py
+++ tsp_ml/scripts/eval_solver_time.py
@@ -21,17 +21,40 @@ from paths import (
 )
 
 DATASET_NAME = "KEP"
+START_WITH_NUM_NODE = 15
 
 if __name__ == "__main__":
     max_instance_size = 150  # num_nodes
     df_cols = ["num_nodes", "instance_id", "prediction_elapsed_time"]
     df = pd.DataFrame(columns=df_cols)
     csv_filepath = RESULTS_FOLDER_PATH / "recursive_solver_time.csv"
-    for num_nodes in range(max_instance_size + 1)[5:]:
+    for num_nodes in range(max_instance_size + 1)[START_WITH_NUM_NODE:]:
         step = f"solver_exp/{num_nodes}"
+        # get folder paths
         kep_dataset_dir = get_dataset_folder_path(dataset_name=DATASET_NAME, step=step)
-        pred_times_list = []
-        for i, filepath in enumerate(kep_dataset_dir.iterdir()):
+        predictions_dir = get_predictions_folder_path(
+            dataset_name=DATASET_NAME,
+            step=step,
+            trained_model_name="RecursiveSolver",
+        )
+        predicted_instances_dir = predictions_dir / "predicted_instances"
+        predicted_instances_dir.mkdir(parents=True, exist_ok=True)
+        # run solver only for the instances that were not yet ran
+        all_instances = [filepath.name for filepath in kep_dataset_dir.iterdir()]
+        predicted_instances = [
+            "kep_instance_" + filepath.name[:-8] + filepath.name[-3:]
+            for filepath in predicted_instances_dir.iterdir()
+        ]
+        remaining_instances = [
+            instance_filename
+            for instance_filename in all_instances
+            if instance_filename not in predicted_instances
+        ]
+        for i, filename in enumerate(remaining_instances):
+            filepath = kep_dataset_dir / filename
+            print(
+                f"[num_nodes=={num_nodes}, {i+1}/{len(remaining_instances)}] solving: {filepath}"
+            )
             # load KEP instance
             kep_instance = torch.load(filepath)
             # use solver, measure time
@@ -40,33 +63,20 @@ if __name__ == "__main__":
             end_time = time.time()
             prediction_elapsed_time = end_time - start_time  # in seconds
             # save prediction
-            predictions_dir = get_predictions_folder_path(
-                dataset_name=DATASET_NAME,
-                step=step,
-                trained_model_name="RecursiveSolver",
-            )
-            predicted_instances_dir = predictions_dir / "predicted_instances"
-            predicted_instances_dir.mkdir(parents=True, exist_ok=True)
             solved_instance_filepath = predicted_instances_dir / (
                 solved_instance.id + "_pred.pt"
             )
             torch.save(solved_instance, solved_instance_filepath)
-            print(
-                f"[num_nodes=={num_nodes}, {i+1}/100] Saved {solved_instance_filepath}"
-            )
+            print(f"Saved {solved_instance_filepath}")
             # collect elapsed time info
             pred_time_dict = {
                 "num_nodes": num_nodes,
                 "instance_id": solved_instance.id,
                 "prediction_elapsed_time": prediction_elapsed_time,
             }
-            pred_times_list.append(pred_time_dict)
-
-            # df = df.append(pred_time_dict, ignore_index=True)
-        df_new_row = pd.DataFrame.from_records(pred_times_list)
-        df = pd.concat([df, df_new_row])
-        print(
-            f"\n\n\nDEBUG mean time per instance size: {df.groupby('num_nodes')['prediction_elapsed_time'].mean()}\n\n"
-        )
-        # save time info (append if it already exist)
-        df.to_csv(csv_filepath, mode="a", header=not os.path.exists(csv_filepath))
+            df = pd.DataFrame.from_records([pred_time_dict])
+            # print(
+            #     f"\n\n\nDEBUG mean time per instance size: {df.groupby('num_nodes')['prediction_elapsed_time'].mean()}\n\n"
+            # )
+            # save time info (append if it already exist)
+            df.to_csv(csv_filepath, mode="a", header=not os.path.exists(csv_filepath))
